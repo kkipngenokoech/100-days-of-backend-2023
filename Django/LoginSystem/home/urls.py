c457f6fff7097b48d273daac0bf78ebebd8f4114
kkipngenokoech <kkipngenokoech22@gmail.com>
2023-01-06 23:07:23 +0300

    static files are working

--- Django/LoginSystem/home/urls.py
+++ Django/LoginSystem/home/urls.py
@@ -4,5 +4,6 @@ from home import views
 urlpatterns = [
     path('home/', views.home, name='home'),
     path("logout/",views.LogoutView,name = "logout"),
-    path("signup/",views.SignupView.as_view(),name ="signup")
+    path("signup/",views.SignupView.as_view(),name ="signup"),
+    path("changepassword/<int:pk>/",views.PasswordChangeView.as_view(),name ='change_password'),
 ]
--- Django/LoginSystem/home/views.py
+++ Django/LoginSystem/home/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, HttpResponse, redirect
 from django.urls import reverse_lazy
 from django.contrib.auth import logout
 from django.contrib.auth.forms import UserCreationForm
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
+from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth.models import User
 # Create your views here.
 def home(request):
     return render(request,'home.html')
@@ -15,3 +17,6 @@ class SignupView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = "registration/signup.html"
+
+class PasswordChangeView(PasswordChangeView):
+    success_url = reverse_lazy('login')
