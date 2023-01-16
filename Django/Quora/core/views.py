f3746e482d70f697584c0e4adfb037f2980a53e6
kkipngenokoech <kkipngenokoech22@gmail.com>
2023-01-16 19:17:10 +0300

    django views

--- Django/Quora/core/views.py
+++ Django/Quora/core/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from . import forms
+from django.contrib.auth import login
+from django.contrib.auth.hashers import make_password
+from django.urls import reverse
 # Create your views here.
+
+class RegisterView(forms.FormView):
+    def get(self, request):
+        content = {}
+        content['form'] = forms.RegisterForm
+        return render(request, 'register.html', {content})
+    def post(self, request):
+        content = {}
+        form = forms.RegisterForm(request.POST, request.FILES or None)
+        if form.is_valid():
+            user = form.save(commit = False)
+            user.password = make_password(form.cleaned_data['password'])
+            user.save()
+            login(request, user)
+            return redirect(reverse('dashboard-view'))
+        content['form'] = form
+        template = 'register.html'
+        return render(request, template, content)
