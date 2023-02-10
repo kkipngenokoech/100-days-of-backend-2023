a3dcb97828062236fbf9bd141e2b560883f48345
kkipngenokoech <kkipngenokoech22@gmail.com>
2023-02-10 07:33:39 +0300

    about page

--- Node/NodeWebsite/index.js
+++ Node/NodeWebsite/index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
+app.get('/about',(req, res) => {
+    res.render('about')
+})
+
 app.listen(3000, () => {
     console.log('Website is up and running on http://localhost:3000')
 })
