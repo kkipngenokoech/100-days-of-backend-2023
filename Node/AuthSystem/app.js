d3a40833110db821ad207d0f3839825e5cb0a7c3
kkipngenokoech <kkipngenokoech22@gmail.com>
2023-02-18 20:57:30 +0300

    importing code

---
+++ Node/AuthSystem/app.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const path = require('path')
+const session = require('express-session')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const mongoose = require('mongoose')
