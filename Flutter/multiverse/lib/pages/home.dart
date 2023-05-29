ddc186679816365bca606ffb92ac8adc78dfd658
kip <kkipngenokoech22@gmail.com>
2023-05-29 19:47:36 +0300

    dart home.dart

---
+++ Flutter/multiverse/lib/pages/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
